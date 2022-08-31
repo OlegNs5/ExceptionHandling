a71e20cf10687a33cf2557608c224be5815dcc65
Windows 10 <irobatic28@gmail.com>
2022-08-31 06:36:10 +0300

    exceptii cu try catch scanner.next line

---
+++ src/ExceptionHandling.java
@@ -0,0 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Scrie un input de la tastatura!");
+        int counter = 0;
+        while(scanner.hasNext()&& counter < 10){
+            try {
+                System.out.println("Numarul " + counter + " citit este: " + scanner.nextDouble());// metoda "nextLine" orice din linia urmatoare
+                System.out.println("Scrie un numar de la tasratura!");
+                counter++;
+            }catch(InputMismatchException ime){
+                System.out.println("Inputul de la tastatura este invalid");
+                scanner.next();
+            }
+        }
+    }
+}
